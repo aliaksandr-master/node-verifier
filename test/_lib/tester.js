51015af5594c45185c35726e65003d0a92ad5d2c
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-16 02:51:54 +0300

    add tester

---
+++ test/_lib/tester.js
@@ -0,0 +1,48 @@
+"use strict";
+
+require('colors');
+var _ = require('lodash');
+var Verifier = require('../../index');
+var iterate = require('../../lib/iterate');
+
+var inspect = function (value) {
+	return '\n' + require('util').inspect(value, {depth: null, colors: true}) + '\n';
+};
+
+var tester = function (cases) {
+
+	return function (test) {
+		iterate.array(cases, function (testCase, index, done) {
+			var verifier;
+			try {
+				verifier = new Verifier(testCase.rules);
+			} catch (e) {
+				if (testCase.error) {
+					done();
+					return;
+				}
+
+				done(e);
+				return;
+			}
+
+			verifier.verify(testCase.value, function (err) {
+				var isValid = !err;
+				if (err) {
+					if (testCase.expect != null && !testCase.expect && err instanceof Verifier.Rule.ValidationError) {
+						done();
+						return;
+					}
+
+					console.log('unexpected error Error('.red, err instanceof Error, ') \n>>'.red, err, '\n', inspect(testCase).cyan);
+					return done(err);
+				}
+
+				test.equal(isValid, testCase.expect, testCase.message || inspect(testCase));
+				done();
+			});
+		}, test.done.bind(test));
+	};
+};
+
+module.exports = tester;

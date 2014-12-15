40ad33267a08757059d2c5bbf87e12c1faa686d8
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-16 02:51:04 +0300

    add new Rule new class

--- rules/base/rule.js
+++ rules/base/rule.js
@@ -1,122 +1,104 @@
 "use strict";
 
-var _ = require('lodash');
-var util = require('util');
-var Class = require('../../lib/class');
-var ValidationError = require('../../errors/validation');
-var ParamError = require('../../errors/param');
-var ValueError = require('../../errors/value');
-var tryit = require('../../lib/tryit');
+var _ = require('./../../lib/utils');
+var extend = require('./../../lib/extend');
 
-var Rule = Class.extend({
+var Rule = function ValidationRule (params) {
+	this.params = this.prepareParams(params);
+};
 
-	name: null,
+Rule.prototype = {
 
 	params: null,
 
-	message: null,
-
-	_error: null,
-
-	initialize: function (params) {
-		this['name'] = this['name'];
-		this.params = this.prepareParams(params == null ? null : params);
-
-		var paramsError;
-		if (this.params instanceof Error) {
-			paramsError = new ParamError(this.params.message);
-		}
-
-		paramsError = paramsError || this.checkParams(this.params);
-
-		if (paramsError && !this._error) {
-			this._error = paramsError instanceof Error ? paramsError : new ParamError('invalid params ' + util.inspect(params, {depth: null}) + '. ' + paramsError);
-		}
-	},
-
 	prepareParams: function (params) {
-		return params;
-	},
-
-	checkParams: function (params) {},
-
-	checkValue: function (value) {},
-
-	verify: function (value, options, done) {
-		var that = this;
-
-		if (this._error) {
-			done(this._error);
-			return;
-		}
-
-		var valueError = this.checkValue(value, options);
-		if (valueError) {
-			done(valueError instanceof Error ? valueError : new ValueError('invalid value ' + util.inspect(value, {depth: null}) + '. ' + valueError));
-			return;
-		}
-
-		tryit(this, this.test, value, this.params, options, function (err, isValid) {
-			if (err && err instanceof ValidationError) {
-				isValid = false;
-			}
-
-			if (!err) {
-				if (isValid === undefined) {
-					isValid = true;
-				}
-
-				if (!isValid) {
-					err = new ValidationError({name: that.name, params: that.params, value: true}, that.message);
-				}
-			}
-
-			done(err, Boolean(isValid));
-		});
-
-		return that;
+		return params == null ? null : params;
 	},
 
 	test: function (value, params, done) {
 		done(new Error('method test must be defined in validation rule "' + this.name + '"!'));
+	},
+};
+
+Rule.extend = function (proto) {
+	var Parent = this;
+
+	var CustomRule = extend(function CustomRule () {
+		Parent.apply(this, arguments);
+	}, Parent);
+
+	_.extend(CustomRule.prototype, proto);
+
+	CustomRule.__super__ = Parent.prototype;
+
+	return CustomRule;
+};
+
+Rule.rules = {};
+
+Rule.add = function (name, rule, strict) {
+	if (!name || typeof name !== 'string') {
+		throw new Error('rule name must be non-empty string');
 	}
-}, {
-	add: function (name, test) {
-		if (_.isFunction(test)) {
-			return this.extend({
-				name: name,
-				test: test
-			});
-		}
 
-		return this.extend(_.extend({
-			name: name
-		}, test));
-	},
+	if (!_.isFunction(rule) || rule.prototype.hasOwnProperty('_$ruleName') || !rule.prototype.test) {
+		throw new Error('rule "' + name + '" must be constructor (with method test), and can register only once');
+	}
 
-	extend: function (proto, stat) {
-		if (!proto || !proto.name || !proto.name.trim()) {
-			throw new Error('rule name must be specified');
-		}
+	if ((strict == null || strict) && this.rules.hasOwnProperty(name)) {
+		throw new Error('rule "' + name + '" already registered');
+	}
 
-		if (!_.isFunction(proto.test)) {
-			throw new Error('rule test function must be specified');
-		}
+	rule.prototype._$ruleName = name;
 
-		var Child = Class.extend.call(this, proto, stat);
+	this.rules[name] = rule;
 
-		this.register[proto.name] = Child;
+	return rule;
+};
 
-		return Child;
-	},
+Rule.get = function (name, strict) {
+	if (!name || typeof name !== 'string') {
+		throw new Error('rule name must be non-empty string');
+	}
 
-	options: {
-		interrupt: true,
-		separator: '|'
-	},
+	if (this.rules.hasOwnProperty(name)) {
+		return this.rules[name];
+	}
 
-	register: {}
-});
+	if (strict == null || strict) {
+		throw new Error('unknown rule "' + name + '"');
+	}
+
+	return null;
+};
+
+Rule.create = function (name, params) {
+	var CustomRule = this.get(name);
+	return new CustomRule(params);
+};
+
+var ValidationError = function ValidationError (ruleName, ruleParams, arrayItemIndex) {
+	if (!(this instanceof ValidationError)) {
+		return new ValidationError(ruleName, ruleParams, arrayItemIndex);
+	}
+
+	if (!ruleName || typeof ruleName !== 'string') {
+		throw new TypeError('invalid ruleName, must be non-empty string');
+	}
+
+	Error.call(this);
+	this.type = this.name = 'ValidationError';
+
+	this.ruleName = ruleName;
+	this.ruleParams = _.cloneDeep(ruleParams);
+	this.arrayItemIndex = typeof arrayItemIndex === 'number' && !_.isNaN(arrayItemIndex) ? arrayItemIndex : null;
+
+	return this;
+};
+
+extend(ValidationError, Error);
+
+Rule.ValidationError = ValidationError;
 
 module.exports = Rule;
 

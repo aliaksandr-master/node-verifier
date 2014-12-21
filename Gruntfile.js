0d32089a969c3e03e59d8b096832ecc1ab78fd2a
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-21 16:54:00 +0300

    add test code coverage scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 "use strict";
 
 module.exports = require('grunto')(function(grunt) {
-
 	return {
 		jshint: {
 			options: grunt.file.readJSON('.jshintrc'),
-			models: [
-				'**/*.{js,json}',
-				'!node_modules/**/*.{js,json}'
+			all: [
+				'index.js',
+				'lib/**/*.{js,json}',
+				'Gruntfile.js',
+				'tests/**/*.{js,json}'
 			]
 		}
 	};
-
 });

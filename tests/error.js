98e50ba9c877a8ea6805f68604c58d3ea9c2196d
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-22 12:23:34 +0300

    add tests for errors in init ValidationError and Verifier

--- tests/error.js
+++ tests/error.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var _ = require('lodash');
 var ValidationError = require('./_lib/lib').Rule.ValidationError;
+
 module.exports = {
 	'instance of Error': function (test) {
 		var err1 = new Error('12123');
@@ -16,6 +18,31 @@ module.exports = {
 		test.ok(err2 instanceof Error);
 		test.ok(err2 instanceof ValidationError);
 		test.ok(!(err1 instanceof ValidationError));
+		test.done();
+	},
+
+	'create by function': function (test) {
+		var err1 = new ValidationError('type', 'string', 3);
+		var err2 = ValidationError('type', 'string', 3); // jshint ignore : line
+
+		test.ok(_.isEqual(err1, err2));
+		test.done();
+	},
+
+	'throws error if incorrect rule name': function (test) {
+
+		test.throws(function () {
+			var err = new ValidationError();
+		});
+
+		test.throws(function () {
+			var err = new ValidationError(123123);
+		});
+
+		test.throws(function () {
+			var err = new ValidationError({});
+		});
+
 		test.done();
 	}
 };
--- tests/main.js
+++ tests/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var _ = require('lodash');
 var tester = require('./_lib/tester');
+var Verifier = require('./_lib/lib');
 
 exports.flow = {
 	standard_call: tester([
@@ -16,5 +18,34 @@ exports.flow = {
 		{ rules: ['type string', 'max_length 2'],     value: "1",    expect: true },
 		{ rules: [{type: 'string'}, {max_length: 2}], value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: [{type: 'string'}, {max_length: 2}], value: "1",    expect: true }
-	])
+	]),
+
+	'create by function': function (test) {
+		var verifier1 = new Verifier('type string');
+		var verifier2 = Verifier('type string'); // jshint ignore : line
+
+		test.ok(_.isEqual(verifier1, verifier2));
+		test.done();
+	},
+
+	'throws error if rules is incorrect': function (test) {
+
+		test.throws(function () {
+			var verifier = new Verifier();
+		});
+
+		test.throws(function () {
+			var verifier = new Verifier(123);
+		});
+
+		test.throws(function () {
+			var verifier = new Verifier('');
+		});
+
+		test.doesNotThrow(function () {
+			var verifier = Verifier([]); // jshint ignore : line
+		});
+
+		test.done();
+	}
 };

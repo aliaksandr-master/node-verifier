40a34a88cd8bf2622de8e9fd88f75204c86c78d8
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-22 11:13:27 +0300

    add additional conditions for error test

--- tests/error.js
+++ tests/error.js
@@ -4,7 +4,14 @@ var ValidationError = require('./_lib/lib').Rule.ValidationError;
 module.exports = {
 	'instance of Error': function (test) {
 		var err1 = new Error('12123');
-		var err2 = new ValidationError('123123');
+		var params = {a: 123};
+		var rule = 'asdasd';
+		var index = 0;
+		var err2 = new ValidationError(rule, params, index);
+
+		test.ok(err2.rule === rule);
+		test.ok(err2.params !== params);
+		test.ok(err2.index === index);
 		test.ok(err1 instanceof Error);
 		test.ok(err2 instanceof Error);
 		test.ok(err2 instanceof ValidationError);

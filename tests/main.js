22aa3953046a7fe511586a54fc801ccb43abd68e
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-22 12:01:18 +0300

    add validation error conditions for main test

--- tests/main.js
+++ tests/main.js
@@ -4,17 +4,17 @@ var tester = require('./_lib/tester');
 
 exports.flow = {
 	standard_call: tester([
-		{ rules: 'type string',                       value: 4,      expect: false },
+		{ rules: 'type string',                       value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: 'type string',                       value: "",     expect: true },
-		{ rules: ['type string'],                     value: 4,      expect: false },
+		{ rules: ['type string'],                     value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: ['type string'],                     value: "",     expect: true },
-		{ rules: {type: 'string'},                    value: 4,      expect: false },
+		{ rules: {type: 'string'},                    value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: {type: 'string'},                    value: "",     expect: true },
-		{ rules: [{type: 'string'}],                  value: 4,      expect: false },
+		{ rules: [{type: 'string'}],                  value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: [{type: 'string'}],                  value: "",     expect: true },
-		{ rules: ['type string', 'max_length 2'],     value: 4,      expect: false },
+		{ rules: ['type string', 'max_length 2'],     value: 4,      verr: { rule: 'type', params: 'string', index: null } },
 		{ rules: ['type string', 'max_length 2'],     value: "1",    expect: true },
-		{ rules: [{type: 'string'}, {max_length: 2}], value: 4,      expect: false },
-		{ rules: [{type: 'string'}, {max_length: 2}], value: "1",    expect: true },
+		{ rules: [{type: 'string'}, {max_length: 2}], value: 4,      verr: { rule: 'type', params: 'string', index: null } },
+		{ rules: [{type: 'string'}, {max_length: 2}], value: "1",    expect: true }
 	])
 };

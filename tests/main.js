54e03df2cc8f3ce013f723bd4e18fc0b77dbbb59
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-22 21:44:20 +0300

    add tests for main use. rule extends, register

--- tests/main.js
+++ tests/main.js
@@ -46,6 +46,78 @@ exports.flow = {
 			var verifier = Verifier([]); // jshint ignore : line
 		});
 
-		test.done();
+		test.throws(function () {
+			var CustomRule = new Verifier.Rule.extend();
+		});
+
+		test.throws(function () {
+			var CustomRule = new Verifier.Rule.extend( { name: 'some' } );
+		});
+
+		var error = new Error('hello');
+		var CustomRule = Verifier.Rule.extend({
+			check: function (value, params, done) {
+				return error;
+			}
+		});
+
+		Verifier.Rule.add("some", CustomRule);
+
+		test.throws(function () {
+			Verifier.Rule.add();
+		});
+
+		test.throws(function () {
+			Verifier.Rule.add(123);
+		});
+
+		test.doesNotThrow(function () {
+			Verifier.Rule.add("some", CustomRule, false);
+		});
+
+		test.throws(function () {
+			Verifier.Rule.add("some");
+		});
+
+		test.throws(function () {
+			Verifier.Rule.add("some1", 123);
+		});
+
+		test.throws(function () {
+			Verifier.Rule.add("some1", function () {});
+		});
+
+		test.throws(function () {
+			var f = function () {};
+			f.prototype = { check: function () {} };
+			Verifier.Rule.add("some1", function () {});
+		});
+
+		test.throws(function () {
+			Verifier.Rule.get('some2');
+		});
+
+		test.doesNotThrow(function () {
+			Verifier.Rule.get('some2', false);
+		});
+
+		var rule = new CustomRule();
+
+		var MyCustomRule = CustomRule.extend({
+			check: function () {
+				throw error;
+			}
+		});
+
+		var myRule = new MyCustomRule();
+
+		rule.verify(123, function (err) {
+			test.ok(err === error);
+
+			myRule.verify(234, function (err) {
+				test.ok(err === error);
+				test.done();
+			});
+		});
 	}
 };

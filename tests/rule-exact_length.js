fbe314fe3595acb8a1d5f2a2d1f901ee83ebaa1f
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-24 22:08:02 +0300

    add error test for rule-exact_lenth test

--- tests/rule-exact_length.js
+++ tests/rule-exact_length.js
@@ -5,91 +5,127 @@ var tester = require('./_lib/tester');
 exports.examples = tester([
 	{
 		rules: 'exact_length 3',
-		 value: 4,
-		 expect: false
+		value: 4,
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: "",
-		 expect: false
+		value: "",
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: "111",
-		 expect: true
+		value: "111",
+		expect: true
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: 111,
-		 expect: true
+		value: 111,
+		expect: true
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: NaN,
-		 expect: false
+		value: NaN,
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: null,
-		 expect: false
+		value: null,
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: undefined,
-		 expect: false
+		value: undefined,
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: {},
-		 expect: false
+		value: {},
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: [],
-		 expect: false
+		value: [],
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 
 	{
 		rules: 'exact_length 3',
-		 value: [ 1, 2, 3 ],
-		 expect: true
+		value: [ 1, 2, 3 ],
+		expect: true
 	},
 		
 	{
 		rules: 'exact_length 3',
-		 value: function () {},
-		 expect: false
+		value: function () {},
+		verr: {
+			rule: 'exact_length',
+			params: 3,
+			index: null
+		}
 	},
 		
 	{
 		rules: 'exact_length -2',
-		 value: '',
-		 error: 'REXACTLENGTH1'
+		value: '',
+		error: 'REXACTLENGTH1'
 	},
 		
 	{
 		rules: 'exact_length -2',
-		 value: 'asdasd',
-		 error: 'REXACTLENGTH1'
+		value: 'asdasd',
+		error: 'REXACTLENGTH1'
 	},
 		
 	{
 		rules: 'exact_length 0',
-		  value: '',
-		 expect: true
+		value: '',
+		expect: true
 	},
 		
 	{
 		rules: 'exact_length 0',
-		  value: '123123',
-		 expect: false
+		value: '123123',
+		verr: {
+			rule: 'exact_length',
+			params: 0,
+			index: null
+		}
 	}
 ]);

a21861edf508cca18c2b328a44fbc96e6276759e
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-24 12:44:35 +0300

    add error check to rule-each test

--- tests/rule-each.js
+++ tests/rule-each.js
@@ -15,33 +15,8 @@ exports.examples = tester([
 	},
 	{
 		rules: 'each type object',
-		value: 333,
-		expect: false
-	},
-	{
-		rules: 'each type object',
-		value: [333],
-		expect: false
-	},
-	{
-		rules: 'each type object',
 		value: [{}],
 		expect: 'REACH1'
-	},
-	{
-		rules: 'each type object',
-		value: [{}, []],
-		expect: false
-	},
-	{
-		rules: {each: ['max_value 10', 'max_length 1']},
-		value: [{}],
-		expect: false
-	},
-	{
-		rules: {each: ['type number', 'max_value 10', 'max_length 1']},
-		value: [{}],
-		expect: false
 	},
 	{
 		rules: {each: ['type number', 'max_value 10', 'max_length 1']},
@@ -49,8 +24,66 @@ exports.examples = tester([
 		expect: true
 	},
 	{
+		rules: 'each type object',
+		value: 333,
+		verr: {
+			rule: 'each',
+			params: [{type: 'object'}],
+			index: null
+		}
+	},
+	{
+		rules: {'each': ['type object', 'not empty']},
+		value: [{}],
+		verr: {
+			rule: 'each',
+			params: { not: [ { empty: null } ] },
+			index: 0
+		}
+	},
+	{
+		rules: 'each type object',
+		value: [333],
+		verr: {
+			rule: 'each',
+			params: { type: 'object' },
+			index: 0
+		}
+	},
+	{
+		rules: 'each type object',
+		value: [{}, []],
+		verr: {
+			rule: 'each',
+			params: { type: 'object' },
+			index: 1
+		}
+	},
+	{
+		rules: {each: ['max_value 10', 'max_length 1']},
+		value: [{}],
+		verr: {
+			rule: 'each',
+			params: { max_value: 10 },
+			index: 0
+		}
+	},
+	{
+		rules: {each: ['type number', 'max_value 10', 'max_length 1']},
+		value: [{}],
+		verr: {
+			rule: 'each',
+			params: { type: 'number' },
+			index: 0
+		}
+	},
+	{
 		rules: {each: ['type number', 'max_value 10', 'max_length 1']},
 		value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-		expect: false
+		verr: {
+			rule: 'each',
+			params: { max_length: 1 },
+			index: 9
+		}
 	}
 ]);

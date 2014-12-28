9bb6037e545dc9ff143fa8a73adc111078f14117
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-28 05:07:39 +0300

    add tests for available_fields

---
+++ tests/rule-available_fields.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var tester = require('./_lib/tester');
+
+exports.examples = tester([
+	{
+		rules: {
+			available_fields: null
+		},
+		value: null,
+		expect: false
+	},
+	{
+		rules: {
+			available_fields: [123, 234, 345]
+		},
+		value: {},
+		expect: true
+	},
+	{
+		rules: {
+			available_fields: [123, 234, 345]
+		},
+		value: {
+			"123": 123123
+		},
+		expect: true
+	},
+	{
+		rules: {
+			available_fields: [123, 234, 345]
+		},
+		value: {
+			"123": 123123,
+			"234": 234234
+		},
+		expect: true
+	},
+	{
+		rules: {
+			available_fields: [123, 234, 345]
+		},
+		value: {
+			"123": 123123,
+			"345": 345345,
+			"234": 234234
+		},
+		expect: true
+	},
+	{
+		rules: {
+			available_fields: [123, 234, 345]
+		},
+		value: {
+			"123": 123123,
+			"345": 345345,
+			"456": 345345,
+			"234": 234234
+		},
+		verr: {
+			rule: 'available_fields',
+			params: ['123', '234', '345']
+		}
+	}
+]);
